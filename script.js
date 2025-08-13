2b7e1007f012c53d17e68919537443d6c2eb938e
Azael <59912489+Wr0t3ByN0b0dy@users.noreply.github.com>
2025-08-13 21:10:21 +0200

    Add new vacation images and update gallery
    Added urlaub8.jpg to urlaub16.jpg to the images folder and updated script.js to include these in the gallery. Improved image display in style.css by adding object-fit: cover for better thumbnail presentation.

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ let imgArray = [
   "urlaub5.jpg",
   "urlaub6.jpg",
   "urlaub7.jpg",
+  "urlaub8.jpg",
+  "urlaub9.jpg",
+  "urlaub10.jpg",
+  "urlaub11.jpg",
+  "urlaub12.jpg",
+  "urlaub13.jpg",
+  "urlaub14.jpg",
+  "urlaub15.jpg",
+  "urlaub16.jpg",
 ];
 
 function initRenderImgs() {
   for (let index = 0; index < imgArray.length; index++) {
-    const element = imgArray[index];
+    const IMG = imgArray[index];
 
     IMAGES_CONTAINER.innerHTML += `<button class="img-button" onclick="showImg()">
-      <img class="main-img" src="./img/pictures/${element}" alt="">
+      <img class="main-img" src="./img/pictures/${IMG}" alt="">
      </button>`;
   }
 }
